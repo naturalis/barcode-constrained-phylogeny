201501c80005fc3679c34fffff84c42a02f7656c
Naomi van Es <70514560+Naomivanes@users.noreply.github.com>
2023-02-07 15:55:31 +0100

    Mapped barcode foreign id (taxon_id) to taxon table

--- src/alter_tables.py
+++ src/alter_tables.py
@@ -1,9 +1,11 @@
 import sqlite3
+# add user arguments
 
 
-def make_db(conn, cursor):
+def make_tables(conn, cursor):
     # Create a table
     cursor.execute("""CREATE TABLE IF NOT EXISTS taxon (
+        taxon_id INTEGER PRIMARY KEY,
         taxon TEXT,
         kingdom TEXT NOT NULL,
         family TEXT NOT NULL
@@ -12,32 +14,44 @@ def make_db(conn, cursor):
     # Create a table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS barcode (
+        barcode_id INTEGER PRIMARY KEY,
         processid TEXT,
         marker_code TEXT,
         nucraw TEXT,
         country TEXT,
-        taxon TEXT
+        taxon_id TEXT,
+        FOREIGN KEY (taxon_id) REFERENCES taxon(taxon_id)
     )""")
     # Commit the changes
     conn.commit()
 
 def make_distinct(conn, cursor):
-    cursor.execute("""INSERT INTO taxon SELECT DISTINCT * FROM taxon_temp""")
-    cursor.execute("""INSERT INTO barcode SELECT DISTINCT * FROM barcode_temp""")
+    cursor.execute("""INSERT INTO taxon (taxon, kingdom, family)
+     SELECT DISTINCT * FROM taxon_temp""")
+
+    # Get taxon_id from taxon table as foreign key insert
+    cursor.execute("""
+     INSERT INTO barcode (processid, marker_code, nucraw, country, taxon_id) 
+     SELECT DISTINCT barcode_temp.processid, barcode_temp.marker_code,
+     barcode_temp.nucraw, barcode_temp.country, taxon.taxon_id
+     FROM barcode_temp INNER JOIN taxon ON barcode_temp.taxon = taxon.taxon""")
     cursor.execute("""DROP TABLE taxon_temp""")
     cursor.execute("""DROP TABLE barcode_temp""")
     # Commit the changes
     conn.commit()
 
+
 if __name__ == '__main__':
     # Connect to the database (creates a new file if it doesn't exist)
-    conn = sqlite3.connect("bold_COI_barcodes.db")
+    conn = sqlite3.connect("BOLD_COI_barcodes.db")
 
     # Create a cursor
     cursor = conn.cursor()
 
     # Make SQLite tables
-    make_db(conn, cursor)
+    make_tables(conn, cursor)
+
+    # Keep only distinct barcodes and taxon
     make_distinct(conn, cursor)
 
     # Close the connection
