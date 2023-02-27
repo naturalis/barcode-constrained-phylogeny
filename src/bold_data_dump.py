dbde850891ab90d7eba3cf46b3a9b40e7db556a3
Noah <noah-i-ching@live.nl>
2023-02-27 15:13:35 +0100

    Possibility to use multiple markers
    Get dataframe with matK and rbcL at the same time in a file containing the designated markers.

--- src/bold_data_dump.py
+++ src/bold_data_dump.py
@@ -9,37 +9,76 @@ par_path = os.path.abspath(os.path.join(os.pardir))
 
 # User arguments
 parser = argparse.ArgumentParser()
-# FIXME How to do this so that user can give multiple markers?
-parser.add_argument('-marker', default="COI-5P",
-                    help="Which barcode marker(s) to select: COI-5P, MatK,"
-                         " RbcL")
-parser.add_argument('-kingdom', default="Animalia",
-                    help="Which kingdom to filter barcodes from: Animalia"
-                         " or Plantae")
-parser.add_argument('-indir', default=par_path+"/data/mnt/bold_public/"
-                                               "datapackages/recent-data/"
-                                               "BOLD_Public.30-Dec-2022.tsv",
-                    help="BOLD tsv file location")
-parser.add_argument('-db', default="BOLD_COI_barcodes.db",
-                    help="Name of the the database file: {file_name}.db")
-args = parser.parse_args()
+
+def select_marker():
+    print("Select your desired marker. \n"
+          "1) For CO1-5P \n"
+          "2) For matK and rbcL")
+    marker = input("Selected number: ")
+
+    # Use userinput to determine desired marker
+    match int(marker):
+        case 1:
+            desired_marker = "COI-5P"
+
+            # Add argument based on desired marker
+            parser.add_argument('-marker', default=desired_marker,
+                                help="Which barcode marker(s) to select: COI-5P, MatK,"
+                                     " RbcL")
+            kingdom = "Animalia"
+            add_args(kingdom, desired_marker)
+            args = parser.parse_args()
+            return args, desired_marker
+        case 2:
+            kingdom = "Plantae"
+            desired_marker = "matK_rbcL"
+
+            # Add argument based on desired marker
+            parser.add_argument('-marker', action='append', nargs=2, metavar=('matK', 'rbcL'),
+                                help="Which barcode marker(s) to select: COI-5P, MatK,"
+                                     " RbcL")
+            add_args(kingdom, desired_marker)
+            args = parser.parse_args('-marker matK rbcL'.split())
+            return args, desired_marker
+        case _:
+            return "Something went wrong"
 
 
-def extract_bold(conn):
-    for chunk in pd.read_csv(args.indir,quoting=csv.QUOTE_NONE,
+def add_args(kingdom, desired_marker):
+    parser.add_argument('-kingdom', default=kingdom,
+                        help="Which kingdom to filter barcodes from: Animalia"
+                             " or Plantae")
+    parser.add_argument('-indir', default=par_path + "/data/mnt/bold_public/"
+                                                     "datapackages/recent-data/"
+                                                     "BOLD_Public.30-Dec-2022.tsv",
+                        help="BOLD tsv file location")
+
+    # Add to file containing the marker
+    parser.add_argument('-db', default="BOLD_{}_barcodes.db".format(desired_marker),
+                        help="Name of the the database file: {file_name}.db")
+
+
+def extract_bold(conn, args, desired_marker):
+    for chunk in pd.read_csv(args.indir, quoting=csv.QUOTE_NONE,
                              low_memory=False, sep="\t", chunksize=10000):
-        #  FIXME How to do this so that user can give multiple markers?
-        # Keep rows that match userarguments
-        df = chunk.loc[
-            (chunk['marker_code'] == args.marker) &
-            (chunk["kingdom"] == args.kingdom)]
+        # Keep rows that match user arguments
+        if desired_marker == "COI-5P":
+            df = chunk.loc[
+                (chunk['marker_code'] == args.marker) & (chunk["kingdom"] == args.kingdom)]
+
+        # args.marker[0][0] is matK and args.marker[0][1] is rbcL
+        else:
+            df = chunk.loc[
+                ((chunk['marker_code'] == args.marker[0][0]) & (chunk["kingdom"] == args.kingdom)) |
+                (chunk['marker_code'] == args.marker[0][1]) & (chunk["kingdom"] == args.kingdom)
+                ]
 
         # Keep stated columns, do not keep rows where NAs are present
         df_temp = df[['taxon', 'kingdom', 'family']].dropna()
 
         # Add rows to SQLite table (makes table if not exitst yet)
         df_temp.to_sql('taxon_temp', conn, if_exists='append',
-                            index=False)
+                       index=False)
 
         # Keep stated columns
         df_temp = df[['processid', 'marker_code', 'nucraw', 'country', 'taxon']]
@@ -52,6 +91,9 @@ def extract_bold(conn):
 
 
 if __name__ == '__main__':
+    # Set args
+    args, desired_marker = select_marker()
+
     # Connect to the database (creates a new file if it doesn't exist)
     conn = sqlite3.connect(args.db)
 
@@ -59,7 +101,7 @@ if __name__ == '__main__':
     cursor = conn.cursor()
 
     # Dump BOLD data into DB
-    extract_bold(conn)
+    extract_bold(conn, args, desired_marker)
 
     # Close the connection
     conn.close()
