ca70b1e7fe183cb1bb3ba8f947288c3f4cae8f69
Naomi van Es <70514560+Naomivanes@users.noreply.github.com>
2023-03-01 13:47:19 +0100

    Write barcode sequences to FASTA file using custom SQLite database. Documented.

--- src/family_fasta.py
+++ src/family_fasta.py
@@ -1,7 +1,6 @@
 import sqlite3
 import argparse
 import os
-
 import numpy as np
 import pandas as pd
 
@@ -9,27 +8,52 @@ par_path = os.path.abspath(os.path.join(os.pardir))
 
 # User arguments
 parser = argparse.ArgumentParser()
-# Add to file containing the marker
+
+# TODO NOAH: change default to how you named your DB or call with commandline
 parser.add_argument('-db', default="BOLD_COI_barcodes.db",
                     help="Name of the the database file: {file_name}.db")
+
 args = parser.parse_args()
 
 
-def divide_fastafiles(conn, cursor):
+def divide_fastafiles(conn):
+    """
+    Takes the barcodes from the SQLite database and divides them into their
+    taxonomic family names. For every family a fasta file is made named
+    'fasta/family/{family name}.fasta'. All barcodes in that family are
+    put in the fasta file with the barcode id from the SQLite db and their
+    nucleotide sequence in FASTA format.
+    :param conn: Connection to SQLite database
+    """
+    # Make directory to put FASTA files in
     os.makedirs('fasta/family', exist_ok=True)
-    df = pd.read_sql_query("SELECT barcode.barcode_id, taxon.family, barcode.nucraw FROM barcode LEFT JOIN taxon ON barcode.taxon_id = taxon.taxon_id LIMIT 10000", conn)
 
-    # Split dataframe into batches and put them in a list
+    # Put needed data from db into a dataframe
+    # TODO Only keep rows where opentol_id in taxon table is not NULL
+    #  This can only be done after the CLB request in map_opentol.py is fixed
+    df = pd.read_sql_query("SELECT barcode.barcode_id, taxon.family, "
+                           "barcode.nucraw FROM barcode LEFT JOIN taxon ON "
+                           "barcode.taxon_id = taxon.taxon_id", conn)
+
+    # Loop through unique family names
     for family in set(df['family']):
+        # Takes about 25 min for COI (7,5 million barcodes)
         print('Making FASTA file for sequences from the family %s...' % family)
-        # Grab records from specific family
+
+        # Grab records from specific family and put them in temp dataframe
         df_family = df[df['family'] == family]
 
+        # Ignore warning
         pd.options.mode.chained_assignment = None
 
-        df_family['fasta'] = df_family['barcode_id'].apply(lambda x: '>' + str(x) + '\n')
+        # Make column with the FASTA header for every barcode
+        df_family['fasta'] = df_family['barcode_id'].apply(
+            lambda barcode_id: '>' + str(barcode_id) + '\n')
+
+        # Add nucraw sequence with the header
         fasta_out = df_family['fasta'] + df_family["nucraw"]
-        # Save BOLD sequences and their respective header in a fastafile
+
+        # Write to FASTA file and name it as their respective family name
         np.savetxt("fasta/family/%s.fasta" % family, fasta_out.values,
                            fmt="%s")
 
@@ -40,7 +64,9 @@ if __name__ == '__main__':
 
     # Create a cursor
     cursor = conn.cursor()
-    divide_fastafiles(conn,cursor)
+
+    # Write barcodes to FASTA in family groups
+    divide_fastafiles(conn)
 
     # Close the connection
     conn.close()
