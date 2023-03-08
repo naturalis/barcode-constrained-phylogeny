7dd633e1fb43a80e663ab96b22f6e8692c0dd115
Naomi van Es <70514560+Naomivanes@users.noreply.github.com>
2023-03-08 16:21:18 +0100

    experimenting

--- src/create_MSA.py
+++ src/create_MSA.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+from Bio.Align.Applications import MuscleCommandline
+
 # Specify paths
 par_path = os.path.abspath(os.path.join(os.pardir))
 masce_path = "../data/macse_v2.06.jar"
@@ -13,9 +15,25 @@ def align_seq():
         output = 'fasta/alignment/' + fasta_file.rstrip('.fasta')
         input = str('fasta/family/' + fasta_file)
         # Uses input FASTA file en generates an alignment in AA and NT
-        alignseq = "java -jar {} -prog alignSequences -seq {} -out_NT {}_NT.fasta -out_AA {}_AA.fasta".format(masce_path, input, output, output)
+        alignseq = "java -jar {} -prog alignSequences -seq {} -out_NT 2 -out {}_NT.fasta -out_AA 2 -out {}_AA.fasta -gap_char -".format(masce_path, input, output, output)
         subprocess.run(alignseq, shell=True)
 
+def align():
+    os.makedirs('alignment', exist_ok=True)
+    for fasta_file in file_list[0:2]:
+
+
+        print("aligning family %s" %fasta_file.rstrip('.fasta'))
+        muscle_cline=MuscleCommandline(input="fasta/family/%s"%fasta_file,out="alignment/alignment_%s"%fasta_file,fasta=True)
+        print(muscle_cline)
+        #muscle_cline()
+        child = subprocess.run(str(muscle_cline),shell=True)
+
+        #whilenotos.path.exists(os.path.join('alignment/','alignment_%s.fasta'%i.strip())):
+        #time.sleep(1)
+        #print('Done')
 
 if __name__ == '__main__':
-    align_seq()
+    # align_seq()
+
+    align()
--- src/create_tree.py
+++ src/create_tree.py
@@ -1,4 +1,6 @@
 import os
+import sqlite3
+import subprocess
 
 from Bio import AlignIO
 from Bio import SeqIO
@@ -9,17 +11,19 @@ from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
 par_path = os.path.abspath(os.path.join(os.pardir))
 os.makedirs('phylip/', exist_ok=True)
 os.makedirs('trees/upgma/', exist_ok=True)
+import opentree
 
 
 def convert_file(fasta_file, phylip_file):
+    print('converting files')
     # Convert fasta to a phylip type file
     #TODO Change this in family_fasta.py so that there are no single sequence fasta files.
     count = 0
-    with open ('fasta/alignment/' + fasta_file, "r") as f:
+    with open (fasta_file, "r") as f:
         for record in SeqIO.parse(f, 'fasta'):
             count += 1
             if count > 1:
-                input_f = SeqIO.parse('fasta/alignment/' + fasta_file, "fasta")
+                input_f = SeqIO.parse(fasta_file, "fasta")
                 SeqIO.write(input_f, phylip_file, "phylip")
                 return True
     return False
@@ -43,12 +47,47 @@ def create_tree(constr, distance_matrix, family):
         f.write(tree.format('newick'))
     return tree
 
+def create_tree_raxml(file_name):
+    print('creating trees')
+    print(file_name)
+    command = "raxmlHPC -s {} -n {} -f d -g {} -m GTRCAT -p 12345"
+    file = 'phylip/alignment_Acanthosomatidae.phylip'
+    outfile = '{}.tre'.format(file_name)
+
+
+    # Set up the OpenTree API client
+    tree_constraint = opentree.OT.synth_subtree(ott_id=215958)
+
+
+    subprocess.run(command.format(file_name + '.fasta', 'tree', tree_constraint), shell=True)
+
+def change_ids(fasta_file):
+    os.makedirs('corrected/', exist_ok=True)
+    from Bio import SeqIO
+    # Connect to the database (creates a new file if it doesn't exist)
+    conn = sqlite3.connect('BOLD_COI_barcodes.db')
+    print(fasta_file)
+    # Create a cursor
+    cursor = conn.cursor()
+    with open("corrected/{}".format(fasta_file), "w") as outputs:
+        for record in SeqIO.parse('alignment/' + fasta_file, "fasta"):
+            result = cursor.execute("SELECT taxon.opentol_id FROM taxon LEFT JOIN barcode ON "
+                                   "barcode.taxon_id = taxon.taxon_id WHERE barcode.barcode_id = {}".format(record.id))
+            id = result.fetchall()[0][0]
+            record.id = '{}_{}'.format(record.id, id)
+            SeqIO.write(record, outputs, 'fasta')
 
 if __name__ == '__main__':
-    file_list = os.listdir(par_path + "/src/fasta/alignment/")
-    for fasta_file in file_list:
-        phylip_file = f"phylip/{fasta_file.rstrip('.fasta')}.phylip"
-        passed = convert_file(fasta_file, phylip_file)
-        if passed:
-            constructor, distance_matrix = create_distance_matrix(phylip_file)
-            tree = create_tree(constructor, distance_matrix, fasta_file.rstrip('.fasta'))
+    file_list = os.listdir(par_path + "/src/alignment/")
+    # for fasta_file in file_list:
+    #     phylip_file = f"phylip/{fasta_file.rstrip('.fasta')}.phylip"
+    #     passed = convert_file(fasta_file, phylip_file)
+    #     if passed:
+    #         constructor, distance_matrix = create_distance_matrix(phylip_file)
+    #         tree = create_tree(constructor, distance_matrix, fasta_file.rstrip('.fasta'))
+    fasta_file = 'alignment_Acanthosomatidae.fasta'
+    phylip_file = f"phylip/{fasta_file.rstrip('.fasta')}.phylip"
+    change_ids(fasta_file)
+    # convert_file('corrected/alignment_Acanthosomatidae.fasta', phylip_file)
+    create_tree_raxml('corrected/'+ fasta_file.rstrip('.fasta'))
+    # change_ids(fasta_file)
