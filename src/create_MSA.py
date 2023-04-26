9c7de77e08ecd56989c2506ca1d889f5502ee806
NI-S <noah.i.ching.scheffer@gmail.com>
2023-04-26 16:30:33 +0200

    Update create_MSA.py
    working with snakemake

--- src/create_MSA.py
+++ src/create_MSA.py
@@ -1,40 +1,33 @@
 import os
 import subprocess
 
-from Bio.Align.Applications import MuscleCommandline
-
-# Specify paths
-par_path = os.path.abspath(os.path.join(os.pardir))
-masce_path = "../data/macse_v2.06.jar"
-file_list = os.listdir(par_path + "/src/fasta/family/")
-
-def align_seq():
-    os.makedirs('fasta/alignment', exist_ok=True)
+def align_seq(in_file, out_file_nt, out_file_aa):
+    os.makedirs('data/fasta/alignment', exist_ok=True)
     #TODO change from two family test data to everything?
-    for fasta_file in file_list[0:2]:
-        output = 'fasta/alignment/' + fasta_file.rstrip('.fasta')
-        input = str('fasta/family/' + fasta_file)
-        # Uses input FASTA file en generates an alignment in AA and NT
-        alignseq = "java -jar {} -prog alignSequences -seq {} -out_NT 2 -out {}_NT.fasta -out_AA 2 -out {}_AA.fasta -gap_char -".format(masce_path, input, output, output)
-        subprocess.run(alignseq, shell=True)
+    # Uses input FASTA file en generates an alignment in AA and NT
+    out_file_nt_temp = out_file_nt.split('_')[0] + "_temp_NT.fasta"
+    out_file_aa_temp = out_file_nt.split('_')[0] + "_temp_AA.fasta"
+    alignseq = "java -jar {} -prog alignSequences -seq {} -out_NT {} -out_AA {}".format(masce_path, in_file,
+                                                                                        out_file_nt_temp,
+                                                                                        out_file_aa_temp)
+    subprocess.run(alignseq, shell=True)
+    remove_exclamation_mark(out_file_nt, out_file_nt_temp) # Call function to remove exclamation mark
+    remove_exclamation_mark(out_file_aa, out_file_aa_temp)
 
-def align():
-    os.makedirs('alignment', exist_ok=True)
-    for fasta_file in file_list[0:2]:
 
+def remove_exclamation_mark(out_file, outfile_temp):
+    """Remove eclamation mark from file.
+    :param output: File to be changed.
+    :return: None, but create a file without exclamation marks.
+    """
+    subprocess.run("sed s/!/-/g {} > {}".format(outfile_temp, out_file), shell=True)
+    os.remove("{}".format(outfile_temp))
 
-        print("aligning family %s" %fasta_file.rstrip('.fasta'))
-        muscle_cline=MuscleCommandline(input="fasta/family/%s"%fasta_file,out="alignment/alignment_%s"%fasta_file,fasta=True)
-        print(muscle_cline)
-        #muscle_cline()
-        child = subprocess.run(str(muscle_cline),shell=True)
-
-        #whilenotos.path.exists(os.path.join('alignment/','alignment_%s.fasta'%i.strip())):
-        #time.sleep(1)
-        #print('Done')
 
 if __name__ == '__main__':
-    # align_seq()
-
-    align()
+    masce_path = snakemake.input[0]
+    in_file = snakemake.input[1]
+    out_file_nt = snakemake.output[0]
+    out_file_aa = snakemake.output[1]
+    align_seq(in_file, out_file_nt, out_file_aa)
     
