f291b4cc019a92aa6c0623ad4c52b55ccc4b9fd9
NI-S <noah.i.ching.scheffer@gmail.com>
2023-05-03 15:21:24 +0200

    get highest dist and write to file

--- src/distance_matrix.py
+++ src/distance_matrix.py
@@ -1,10 +1,11 @@
 import numpy as np
 from ete3 import Tree       # To use for distance matrix
 import pandas as pd
+import SeqIO
 
 
 def create_matrix():
-    tree = Tree("test/test with ott and selfmade id/Aba.bestTree",
+    tree = Tree("Aba.bestTree",
                 format=1)  # Format indicates newick structure in file
     nodes = list(tree.get_leaves())  # Get all leaves
     names = []
@@ -21,23 +22,56 @@ def create_matrix():
     dist_df = pd.DataFrame(data=dmat, index=names, columns=names)  # Make into dataframe
     highest = dist_df.max()     # Get all highest from rows
     print(max(highest))     # Get highest values
-    result_list = search_coordinate(dist_df, {max(highest)})
-    # Printing the rows and colomns that contain the highest value
-    print(f"\n\n{'row':<4} {'col':<4} {'name':>10}")
-    [print(f"{row:<4} {col:<4} {name:>10}") for row, col, name in result_list]
+    result_list = search_matrix(dist_df, {max(highest)})
+    # Get row and column name
+    get_corresponding_ott(dist_df,result_list[0][0], result_list[0][1])
 
     dist_df.to_csv("../data/tree_distancematrix.txt",
                    sep='\t',
                    header=True)  # Write distance matrix to txt file
 
-def search_coordinate(df_data: pd.DataFrame, search_set: set) -> list:
+
+def search_matrix(df_data: pd.DataFrame, search_set: set) -> list:
     nda_values = df_data.values
     tuple_index = np.where(np.isin(nda_values, [e for e in search_set]))
     return [(row, col, nda_values[row][col]) for row, col in zip(tuple_index[0], tuple_index[1])]
+
+def get_corresponding_ott(df, col_pos, row_pos):
+    representatives = []
+    colname = df.columns[col_pos]
+    rowname = df.rows[row_pos]
+    representatives.append(colname)
+    representatives.append(rowname)
+    return representatives
+
+
+def representatives_to_file(representatives):
+    """Important to get the normal sequences in this file"""
+    dir = "src/fasta/family/"
+    filename = "Abacionidae.fasta"
+    with open("{}/{}".format(dir, filename), "r") as input:
+        with open("representatives.fasta", "w+") as output:
+            for record in SeqIO.parse(input, "fasta"):
+                print(record.id)
+                for seq in representatives:
+                    if record.id == seq:
+                        SeqIO.write(record, output, 'fasta')
 
 
 if __name__ == "__main__":
     create_matrix()
 
+
+    """" 
+    # Make connection to the database
+    conn = sqlite3.connect(database)
+
+    # Create a cursor
+    cursor = conn.cursor()
+
+    # Close the connection
+    conn.close()
+    create_matrix()
+"""
 
 
