905cdc8c42a783015abaeb6681f7b43a7177433b
NI-S <noah.i.ching.scheffer@gmail.com>
2023-05-08 15:24:57 +0200

    representatives to file with snakemake, not yet in snakemake phylogeny file

---
+++ src/representatives_to_file.py
@@ -0,0 +1,21 @@
+from Bio import SeqIO
+def representatives_to_file(representatives, inputfile, output):
+    """The sequences with the highest distance (which are also in de opentree of life db) are written to a fasta file.
+    """
+
+    with open(inputfile, "r") as input:
+        with open(output, "a") as outputs:
+            for record in SeqIO.parse(input, "fasta"):
+                # Iterate over the representatives to find their corresponding sequences
+                for header in representatives:
+                    if record.id == header:
+                        # Write to fasta file
+                        SeqIO.write(record, outputs, 'fasta')
+
+
+if __name__ == '__main__':
+    inputfile = snakemake.input[0]
+    representatives = snakemake.input[1]
+    outputfile = snakemak.output[2]
+    representatives_to_file(representatives, inputfile, outputfile)
+
