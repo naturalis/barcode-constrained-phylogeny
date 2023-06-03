34c15698a1afee09ef619922532440ef4c26e866
Rutger Vos <rutgeraldo@gmail.com>
2023-06-03 13:28:00 +0200

    fixes #48

--- src/map_opentol.py
+++ src/map_opentol.py
@@ -1,187 +1,172 @@
-import numpy as np
-import pandas as pd
 import sqlite3
-import os
 import requests
 import json
 import logging
+import os
+import pandas as pd
+import numpy as np
+
+# Set the Open Tree of Life API endpoint
+# https://github.com/OpenTreeOfLife/germinator/wiki/TNRS-API-v3#match_names
+endpoint = "https://api.opentreeoflife.org/v3/tnrs/match_names"
+
+# Instantiate logger
+logging.basicConfig(level=snakemake.params.log_level)  # noqa: F821
+logger = logging.getLogger(__name__)
 
 
-def map_checklistbank(conn, kingdom):
+def tnrs_request(kingdom, names, fuzzy=False):
     """
-    Maps bold taxon names from the database taxon table to opentol ids.  It only maps the name if it's an exact
-    string match (no fuzzy matching). A context has to be given (args.kingdom) to determine which kingdom the
-    taxon names belong to (Animals or Plants). Saves new table as opentol_temp in the database.
-    :param conn: Connection object to the database.
-    :param kingdom: The kingdom context to match within
+    Performs a taxonomic name resolution (TNRS) request on the OpenToL API for a list of names . The service does this
+    within a given taxonomic context, to specify which code applies (botany or zoology).
+    :param kingdom: Taxonomic context, 'Animals' or 'Plants'
+    :param names: Taxon name to match
+    :param fuzzy: Boolean, if True does approximate matching
+    :return: A result object with the matches, parsed from JSON output
+    """
+
+    tnrs_params = {
+        "names": names,
+        "do_approximate_matching": fuzzy,
+        "verbose": False,
+        "context": kingdom
+    }
+    json_response = requests.post(endpoint, json=tnrs_params)
+    return json.loads(json_response.text)
+
+
+def tnrs_response(matches, score=1.0):
+    """
+    Processes a TNRS response. Accepts responses if and only if there is a single match that is not a synonym whose
+    score is greater than or equal to the score parameter. Returns the OTT ID if successful, None otherwise.
+    :param matches: The data structure resulting from the parsed JSON returned by the TNRS service
+    :param score: A value between 0 and 1 (inclusive)
+    :return: OTT ID or None
+    """
+
+    # There are results
+    if len(matches) != 0:
+
+        # There should be only one result that is not a synonym and whose score is high enough
+        filtered = [d for d in matches if
+                    not d['is_synonym'] and d['score'] >= score and d['taxon']['rank'] == 'species']
+        if len(filtered) == 1:
+            return filtered[0]['taxon']['ott_id']
+        else:
+            logger.debug("No single match: %s", filtered)
+
+    # Either there was no result, or there were too many ambiguous results
+    return None
+
+
+def match_opentol(database, kingdom, chunksize, fuzzy):
+    """
+    Matches names against the OpenTOL. Does this for all records in the taxon table that don't have an opentol_id.
+    Attempts first an exact match, then tries to recover through a fuzzy match if initially there was no match.
+    Updates the database records with matches.
+    :param database: Name of SQLite database
+    :param kingdom: The applicable kingdom to search within.
+    :param chunksize: How many names to check in batch. Literal<=10000, Fuzzy<=250
+    :param fuzzy: If True do fuzzy matching, otherwise literal matching
     :return:
     """
-    # Change column name from taxon to scientificName
-    df = pd.read_sql("SELECT * FROM taxon", conn)
 
-    # Set the Open Tree of Life API endpoint
-    endpoint = "https://api.opentreeoflife.org/v3"
+    # Try all unmatched names in database in chunks
+    conn = sqlite3.connect(database, isolation_level=None)
+    cursor = conn.cursor()
 
-    # Split df into chunks
-    list_df = np.array_split(df, 50)
+    # Create temporary table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS taxon_temp (
+        taxon_id INTEGER PRIMARY KEY,
+        taxon TEXT,
+        kingdom TEXT NOT NULL,
+        family TEXT NOT NULL,
+        genus TEXT NOT NULL,
+        opentol_id INTEGER)""")
 
-    ott_ids = []
-    names = []
-    for taxons in list_df:
-        tnrs_url = f"{endpoint}/tnrs/match_names"
-        # TODO instead of Animals check if its animalia or plantae kingdom and put appropriate context
-        tnrs_params = {
-            "names": taxons['taxon'].to_list(),
-            "do_approximate_matching": False,
-            "verbose": False,
-            "context": kingdom
-        }
-        tnrs_response = requests.post(tnrs_url, json=tnrs_params)
-        tnrs_data = json.loads(tnrs_response.text)
+    # Load all unmatched records into df, iterate over it in chunks
+    df = pd.read_sql("SELECT * FROM taxon WHERE opentol_id IS NULL", conn)
+    for _, chunk_df in df.groupby(np.arange(len(df)) // chunksize):
 
-        # Get the first OTT ID returned from the search
-        for result in tnrs_data['results']:
-            if len(result['matches']) != 0:
-                # Extract the ott_id and name values from the 'taxon' dictionary
-                ott_id = result['matches'][0]['taxon']['ott_id']
-                name = result['matches'][0]['taxon']['unique_name']
-                # Append the values to the corresponding lists
-                ott_ids.append('ott' + str(ott_id))
-                names.append(name)
+        # Check all names within the chunk as a list against TNRS
+        tnrs_data = tnrs_request(kingdom, chunk_df['taxon'].to_list(), fuzzy=fuzzy)
 
-    # Create a Pandas DataFrame from the lists of values
-    df_ot = pd.DataFrame({'ott_id': ott_ids, 'taxon': names})
+        # Process the results, update temp table
+        for i in range(len(tnrs_data['results'])):
 
-    # Left join old table with new table containing opentol IDs
-    df_merged = pd.merge(df, df_ot, left_on='taxon', right_on='taxon', how='left').drop(columns=['opentol_id']) \
-        .rename(columns={'ott_id': 'opentol_id'})
+            # Prepare record to insert, coerce types
+            taxon_id = int(chunk_df['taxon_id'].iloc[i])
+            taxon = str(chunk_df['taxon'].iloc[i])
+            kingdom = str(chunk_df['kingdom'].iloc[i])
+            family = str(chunk_df['family'].iloc[i])
+            genus = str(chunk_df['genus'].iloc[i])
 
-    # Drop duplicate rows
-    df_merged.drop_duplicates(ignore_index=True, inplace=True)
+            # Attempt to get OTT ID
+            ott_id = tnrs_response(tnrs_data['results'][i]['matches'])
+            if ott_id is not None:
+                opentol_id = int(ott_id)
+                data_to_insert = (taxon_id, taxon, kingdom, family, genus, opentol_id)
+                sql_command = """
+                    INSERT INTO taxon_temp (taxon_id, taxon, kingdom, family, genus, opentol_id)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                """
+                cursor.execute(sql_command, data_to_insert)
 
-    # Save df to a table in the db
-    df_merged.to_sql('opentol_temp', conn, if_exists='replace', index=False)
+            else:
+                data_to_insert = (taxon_id, taxon, kingdom, family, genus)
+                sql_command = """
+                    INSERT INTO taxon_temp (taxon_id, taxon, kingdom, family, genus)
+                    VALUES (?, ?, ?, ?, ?)
+                """
+                cursor.execute(sql_command, data_to_insert)
+            conn.commit()
+            logger.info(data_to_insert)
 
-    conn.commit()
-
-
-def map_checklistbank_fuzzy(kingdom):
-    """
-    Maps bold taxon names from the database opentol_temp table to opentol ids. It tries to map all the
-    taxon names that did not got an exact match in map_checklistbank(), using fuzzy matching. BOLD names are
-    replaced with the matched taxon names from OpenTOL. A context has to be given (args.kingdom) to determine
-    which kingdom the taxon names belong to (Animals or Plants). Saves new table as temp in the database.
-    :param conn: Connection object to the database.
-    """
-    df = pd.read_sql("SELECT * FROM opentol_temp WHERE opentol_id IS NULL",
-                     conn)
-
-    # Set the Open Tree of Life API endpoint
-    endpoint = "https://api.opentreeoflife.org/v3"
-
-    # Split df into chunks
-    list_df = np.array_split(df, 50)
-
-    ott_ids = []
-    names = []
-    new_taxons = []
-
-    # Loop through chunks from the df
-    for taxons in list_df:
-        tnrs_url = f"{endpoint}/tnrs/match_names"
-        # TODO instead of Animals check if its animalia or plantea kingdom and put appropiate context
-        tnrs_params = {
-            "names": taxons['taxon'].to_list(),
-            "do_approximate_matching": True,
-            "verbose": False,
-            "context": kingdom
-        }
-        tnrs_response = requests.post(tnrs_url, json=tnrs_params)
-        tnrs_data = json.loads(tnrs_response.text)
-
-        # Loop trough response results
-        for result in tnrs_data['results']:
-            # Check if there was a match found
-            if len(result['matches']) != 0:
-                # Extract the ott_id, bold name and opentol ID  values from the response
-                ott_id = result['matches'][0]['taxon']['ott_id']
-                name = result['matches'][0]['matched_name']
-                new_taxon = result['matches'][0]['taxon']['unique_name']
-
-                # Shows a lsit of flags like incertae_sedis, not used now but might be later
-                flags = result['matches'][0]['taxon']['flags']
-
-                # Append the values to the corresponding lists
-                ott_ids.append('ott' + str(ott_id))
-                names.append(name)
-                new_taxons.append(new_taxon)
-
-    # Create a Pandas DataFrame from the lists of values
-    df_ot = pd.DataFrame({'ott_id': ott_ids, 'taxon': names, 'new_taxon': new_taxons})
-
-    # Left join old table with new table containing opentol IDs
-    df_old = pd.read_sql("SELECT * FROM opentol_temp", conn)
-
-    df_merged = pd.merge(df_old, df_ot, left_on='taxon', right_on='taxon',
-                         how='left')
-
-    # If opentol_id is empty, replace with ott_id
-    df_merged['opentol_id'] = df_merged['opentol_id'].fillna(df_merged['ott_id'])
-
-    # If opentol_id and ott_id are the same, safe mask
-    mask = df_merged['opentol_id'] == df_merged['ott_id']
-
-    # Replace values in column taxon with the new_taxon value, where mask is True
-    df_merged.loc[mask, 'taxon'] = df_merged.loc[mask, 'new_taxon']
-
-    df_merged.drop(columns=['new_taxon', 'ott_id'], inplace=True)
-
-    df_merged.to_sql('temp', conn, if_exists='replace', index=False)
-    conn.commit()
-
-
-def alter_db(conn, cursor):
-    """
-    Drops the old taxon table and opentol_temp table. Renames temp to taxon.
-    :param conn: Connection object to the database.
-    :param cursor: Cursor object to execute SQL commands.
-    """
-    # Remove temporary table
-    cursor.execute("""DROP TABLE opentol_temp""")
+    # Copy over the already matched records
+    cursor.execute("""INSERT INTO taxon_temp SELECT * FROM taxon WHERE opentol_id IS NOT NULL""")
 
     # Remove old taxon table
     cursor.execute("""DROP TABLE taxon""")
 
     # Rename new taxon table from temp to taxon
-    cursor.execute("""ALTER TABLE temp RENAME TO taxon""")
+    cursor.execute("""ALTER TABLE taxon_temp RENAME TO taxon""")
 
-    # Commit changes
+    conn.close()
+
+
+def postprocess_db(database):
+    # create indexes on family, genus, opentol_id, taxon_id country and nucraw
+    conn = sqlite3.connect(database, isolation_level=None)
+    cursor = conn.cursor()
+    cursor.execute("""CREATE INDEX IF NOT EXISTS family_idx ON taxon (family)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS genus_idx ON taxon (genus)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS opentol_id_idx ON taxon (opentol_id)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS taxon_id_idx ON barcode (taxon_id)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS country_idx ON barcode (country)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS nucraw_idx ON barcode (nucraw)""")
     conn.commit()
+    conn.close()
 
 
 if __name__ == '__main__':
     temp_database_name = snakemake.input[0]  # noqa: F821
     marker = snakemake.params.marker  # noqa: F821
     database_file = snakemake.output[0]  # noqa: F821
-    logging.basicConfig(level=snakemake.params.log_level)  # noqa: F821
-    logger = logging.getLogger(__name__)
 
+    # Infer taxonomic context from marker name
     if marker == "COI-5P":
         kingdom = 'Animals'
     else:
         kingdom = "Plants"
-    # Connect to the database (creates a new file if it doesn't exist)
-    conn = sqlite3.connect(temp_database_name)
-    # Create a cursor
-    cursor = conn.cursor()
-    logger.info("Exact matching between BOLD and OpenTOL")
-    map_checklistbank(conn, kingdom)
-    logger.info("Fuzzy matching remaining taxa")
-    map_checklistbank_fuzzy(kingdom)
 
-    # Alter new table and remove old tables
-    alter_db(conn, cursor)
+    # Literal matching in big steps
+    logger.info("Going to match literally in chunks of 10,000 names")
+    match_opentol(temp_database_name, kingdom, chunksize=10000, fuzzy=False)
 
+    # Fuzzy matching in small steps
+    logger.info("Going to match fuzzily in chunks of 250 names")
+    match_opentol(temp_database_name, kingdom, chunksize=250, fuzzy=True)
+
+    # Compute indexes and write to final file
+    postprocess_db(temp_database_name)
     os.rename(temp_database_name, database_file)
-    # Close the connection
-    conn.close()
