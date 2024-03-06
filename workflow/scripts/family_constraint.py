a5f6626b6029319ee9f6549a4cd32102fa3122b9
Rutger Vos <rutgeraldo@gmail.com>
2024-03-06 13:40:16 +0100

    more logging, more comments and docstring, planning for dealing with mrca tips

--- workflow/scripts/family_constraint.py
+++ workflow/scripts/family_constraint.py
@@ -5,19 +5,54 @@ import dendropy
 
 # Function to modify tip labels and remove interior node labels
 def modify_tree_labels(tree):
+    """
+    Processes the tip and node labels. If tip, remove everything but the
+    ott ID. If node, remove the label. If an interior node has been
+    elevated to a tip (which can be recognized by the mrca prefix in the
+    label), prune it.
+    :param tree: a dendropy tree
+    :return:
+    """
+
     # Iterate over all nodes in the tree
     for node in tree.preorder_node_iter():
-        if node.is_leaf():  # Check if it is a terminal node (leaf)
+
+        # Check if it is a terminal node (leaf)
+        if node.is_leaf():
+
             # Modify tip labels to keep only the 'ott+number' part
             label_parts = node.taxon.label.split(' ')
             ott_part = label_parts[-1]
-            node.taxon.label = ott_part
+
+            # Check if MRCA and remove it.
+            # TODO: this situation arises when a taxon is 'broken' according to OpenToL.
+            # For example, requesting _Alouatta seniculus_ results in this because the
+            # species subtends two ssp _A. seniculus_ as well as _A. sara_. The solution
+            # appears to be to parse the mrca string, tease out the ott IDs, then re-request,
+            # so that we can at least figure out the species names (the initial output
+            # doesn't give them) and map that back to our input.
+            if ott_part.startswith('mrca'):
+                logger.warning(f'Tree has MRCA tip in it: {ott_part}')
+                node.parent_node.remove_child(node)
+            else:
+                node.taxon.label = ott_part
         else:
-            # Remove interior node labels (set taxon to None)
+
+            # Remove interior node labels
             node.label = None
 
 
 def fetch_induced_subtree(ids):
+    """
+    Places a request to the OpenToL induced subtree web service endpoint.
+    Parameterized by a list of ott IDs. This service call sometimes fails,
+    when the parameter set includes IDs not in the subtree, which results
+    in a warning being emitted. In that case, the return value includes
+    a list of the unknown IDs. The caller can then remove these from the
+    input list and try again.
+    :param ids: a list of ott IDs
+    :return: a JSON data structure
+    """
     # The API endpoint URL
     url = "https://api.opentreeoflife.org/v3/tree_of_life/induced_subtree"
 
@@ -44,6 +79,13 @@ def fetch_induced_subtree(ids):
 
 
 def extract_id_from_fasta(file_path):
+    """
+    Extracts the ott IDs from a FASTA file. This operates specifically on
+    FASTA headers where the ott ID is the second element (i.e. at index 1)
+    in the pipe-separated FASTA header.
+    :param file_path: the location of a FASTA file
+    :return: a list of ott IDs
+    """
     ids = []
     with open(file_path, 'r') as file:
         for line in file:
@@ -61,6 +103,14 @@ def extract_id_from_fasta(file_path):
 
 
 def postprocess_tree(newick):
+    """
+    Given a newick string as input, parses the string into a dendropy tree,
+    cleans up the tip labels (via modify_tree_labels) and removes unbranched
+    interior nodes
+    :param newick: a newick string
+    :return: a dendropy tree object
+    """
+    logger.debug(f'Post-processing result tree {newick}')
 
     # Read tree from string with dendropy
     tree_obj = dendropy.Tree.get(
@@ -93,11 +143,11 @@ if __name__ == '__main__':
     # Read input alignment, get ott IDs
     ott_ids = extract_id_from_fasta(args.inaln)
 
-    # If we have no IDs at all, we write a :; file
+    # If we have no IDs at all, we write a zero byte file for run_raxml
     if len(ott_ids) == 0:
         logger.warning('There were zero OTT IDs in the input file')
-        with open(args.outtree, "w") as output_file:
-            output_file.write(':;')
+        with open(args.outtree, "a"):
+            pass
 
     else:
         # Get JSON and process it
@@ -106,6 +156,7 @@ if __name__ == '__main__':
         # If it has unknowns in it, run it again
         if 'unknown' in result:
             unknown_ott_ids = [ item.removeprefix('ott') for item in result['unknown'].keys() ]
+            logger.warning(f'Request included unkown ott IDs: {unknown_ott_ids}')
             cleaned_list = [item for item in ott_ids if item not in unknown_ott_ids]
             result = fetch_induced_subtree(cleaned_list)
 
