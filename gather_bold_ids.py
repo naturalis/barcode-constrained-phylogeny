4593b60a1a4516a246d0882a9e84c90d8fa8ea7e
AnnemiekeSchonthaler <annemiekeschonthaler@gmail.com>
2024-07-01 10:34:17 +0200

    script to generate the bold ids of the NSR, and the generated file

--- gather_bold_ids.py
+++ gather_bold_ids.py
@@ -4,79 +4,72 @@ import json
 import time
 from io import StringIO
 
-
-def gather_names(dataframe_taxa):
+def gather_names_NSR(dataframe_taxa):
     list_organisms = []
     for index, row in dataframe_taxa.iterrows():
         list_organisms.append(f"{row['genus']} {row['specificEpithet']}")
-    return list_organisms
+    list_tax_ids = list(dataframe_taxa['taxonID'])
+    return list_organisms, list_tax_ids
 
 
-def get_bold_names(list_organisms):
-    all_bold_ids = []
-    unmatched = []
+def get_bold_organisms(list_organisms):
     count = 0
     length = len(list_organisms)
-    write_out_ids = "NSR name\tTaxID\n"
-    write_out_umatched = "NSR name\n"
+    organisms_bold = []
     for organism in list_organisms:
-        print(f"{count} out of {length}")
+        # print(f"{count} out of {length}")
         organism_split = organism.split(" ")
         genus = organism_split[0]
         species = organism_split[1]
 
+        # checklist bank api
         response = requests.get(rf"https://api.checklistbank.org/dataset/37384/nameusage/search?q={genus}%20{species}&offset=0&limit=10")
         try:
             json_resp = response.json()
-            bold_id = json_resp['result'][0]['usage']['name']['link']
+            organism_name = json_resp['result'][0]['usage']['name']['scientificName']
             # "http://bins.boldsystems.org/index.php/Taxbrowser_Taxonpage?taxid=12439"
-            bold_id = bold_id.split("taxid=")[1]
-            all_bold_ids.append(bold_id)
-            write_out_ids += f"{genus} {species}\t{bold_id}\n"
+            organisms_bold.append(organism_name)
         except:
-            unmatched.append(organism)
-            write_out_ids += f"{genus} {species}\n"
-        if count % 4000 == 0:
-            time.sleep(5)
+            organisms_bold.append(f"{genus} {species}")
         count += 1
-    
-    open("resources/bold_ids.txt", "w").write(write_out_ids)
-    open("resources/unmatched.txt", "w").write(write_out_umatched)
+
+    return organisms_bold
 
 
-def match_unmatched(filename="resources/unmatched.txt"):
-    list_organisms =  open(filename, "r").readlines()[1:]
-    all_bold_ids = []
-    unmatched = []
+def match_unmatched(organisms_bold):
+    # lines = open(filename, "r").readlines()[1:]
     count = 0
-    length = len(list_organisms)
-    write_out_ids = ""
-    write_out_umatched = "NSR name\n"
-    for organism in list_organisms:
-        print(f"{count} out of {length}")
-        organism_split = organism.split(" ")
-        genus = organism_split[0]
-        species = organism_split[1]
-
-        response = requests.get(rf"https://v3.boldsystems.org/index.php/API_Public/specimen?taxon={genus}%20{species}&format=tsv")
+    # length = len(lines)
+    bold_ids = []
+    for organism_name in organisms_bold:
+        # print(f"{count} out of {length}")
+        organism_split = organism_name.split(" ")
         try:
-            gathered_ids = set(pd.read_csv(StringIO(response.text), sep="\t")["species_taxID"].tolist())
-            prepped_id = str(int(list({x for x in gathered_ids if pd.notna(x)})[0]))
-            all_bold_ids.append(prepped_id)
-            write_out_ids += f"{genus} {species}\t{prepped_id}\n"
+            genus = organism_split[0]
+            species = organism_split[1]
+
+            response = requests.get(rf"https://v3.boldsystems.org/index.php/API_Public/specimen?taxon={genus}%20{species}&format=tsv")
+            res = pd.read_csv(StringIO(response.text), sep="\t")
+            gathered_ids = set(res["processid"].tolist())
+            prepped_id = str(list({x for x in gathered_ids if pd.notna(x)})[0])
+            bold_ids.append(prepped_id)
         except:
-            unmatched.append(organism)
-            write_out_umatched += f"{genus} {species}\n"
-        if count % 4000 == 0:
-            time.sleep(5)
+            bold_ids.append("")
         count += 1
-    open("resources/bold_names.txt", "a").write(write_out_ids)
-    open("resources/unmatched.txt", "w").write(write_out_umatched)
+    return bold_ids
 
 
 if __name__ == '__main__':
     filename_nsr_taxa = "resources/Taxa.txt"
     dataframe_taxa = pd.read_csv(filename_nsr_taxa)
-    list_organisms = gather_names(dataframe_taxa)
-    get_bold_names(list_organisms)
-    match_unmatched()
+    list_organisms, list_tax_ids = gather_names_NSR(dataframe_taxa)
+    organisms_bold = get_bold_organisms(list_organisms)
+    bold_ids = match_unmatched(organisms_bold)
+
+    info_dump = {'NSR_organism': list_organisms[0:len(organisms_bold)],
+                    'NSR_ID': list_tax_ids[0:len(organisms_bold)],
+                    'BOLD_organism': organisms_bold[0:len(organisms_bold)],
+                    'BOLD_ID': bold_ids[0:len(organisms_bold)]} 
+    # print(info_dump)
+    df_dump = pd.DataFrame(info_dump)
+    df_dump.to_csv("resources/bold_ids.txt", sep='\t', index=False, header=True)
