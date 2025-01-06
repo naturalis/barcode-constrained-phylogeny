8ba55e7bbb9e7e00563eb64e37c39445c36c206a
dominika.kresa <dominika.kresa@naturalis.nl>
2025-01-06 16:09:42 +0100

    add missing arg in graft clased & wrap fasta seq

--- workflow/scripts/process_failed_file.py
+++ workflow/scripts/process_failed_file.py
@@ -13,10 +13,12 @@ def remove_outgroup_records(failed_file, outgroup_file, output_file):
     with open(failed_file, "r") as failed, open(output_file, "w") as output:
         for record in SeqIO.parse(failed, "fasta"):
             if record.id not in outgroup_records:
-                SeqIO.write(record, output, "fasta")
+                # Ensure sequence is written in one line
+                record.seq = record.seq.replace("-", "")
+                SeqIO.write(record, output, "fasta-2line")  # Use "fasta-2line" to ensure one-line sequences
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process failed file by removing outgroup records.")
+    parser = argparse.ArgumentParser(description="Process failed file by removing outgroup records and ensuring one-line sequences.")
     parser.add_argument("--failed", required=True, help="Path to the failed file (FASTA format).")
     parser.add_argument("--outgroup", required=False, help="Path to the outgroup file (FASTA format).")
     parser.add_argument("--output", required=True, help="Path to the output file (FASTA format).")
