6a56b3f0b4ab82bb9a429749e46f5a67f191c2eb
dominika.kresa <dominika.kresa@naturalis.nl>
2025-01-08 20:20:57 +0100

    fix run_raxml rule

--- workflow/scripts/process_failed_file.py
+++ workflow/scripts/process_failed_file.py
@@ -1,6 +1,7 @@
 import argparse
 from Bio import SeqIO
 
+
 def remove_outgroup_records(failed_file, outgroup_file, output_file):
     # Read outgroup sequences
     outgroup_records = set()
@@ -13,12 +14,14 @@ def remove_outgroup_records(failed_file, outgroup_file, output_file):
     with open(failed_file, "r") as failed, open(output_file, "w") as output:
         for record in SeqIO.parse(failed, "fasta"):
             if record.id not in outgroup_records:
-                # Ensure sequence is written in one line
+                # Ensure a sequence is written in one line
                 record.seq = record.seq.replace("-", "")
                 SeqIO.write(record, output, "fasta-2line")  # Use "fasta-2line" to ensure one-line sequences
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process failed file by removing outgroup records and ensuring one-line sequences.")
+    parser = argparse.ArgumentParser(
+        description="Process failed file by removing outgroup records and ensuring one-line sequences.")
     parser.add_argument("--failed", required=True, help="Path to the failed file (FASTA format).")
     parser.add_argument("--outgroup", required=False, help="Path to the outgroup file (FASTA format).")
     parser.add_argument("--output", required=True, help="Path to the output file (FASTA format).")
